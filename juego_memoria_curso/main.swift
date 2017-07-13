b64d34d83cabb19a7cdb61a6dc6d627f8ac13cf5
Macintosh <macintosh@MacBook-Pro-de-Macintosh.local>
2017-07-13 17:42:28 +0200

    Initial Commit

---
+++ juego_memoria_curso/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  juego_memoria_curso
+//
+//  Created by Macintosh on 13/7/17.
+//  Copyright © 2017 Macintosh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
